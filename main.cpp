f6bb14c107d4cb5986cbd6f46fabca99a4b44135
Jed Jocson <jed.jocson@obf.ateneo.edu>
2022-10-16 17:30:52 +0800

    First commit.

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <raylib.h>;
+
+const int WINDOW_WIDTH(1280);
+const int WINDOW_HEIGHT(720);
+const int TARGET_FPS(60);
+
+const int SMALL_CIRCLES_TO_SPAWN_SIMULTANEOUSLY(25);
+const int SMALL_CIRCLE_RADIUS_MIN(5);
+const int SMALL_CIRCLE_RADIUS_MAX(10);
+const int SMALL_CIRCLE_MASS(1);
+
+const int NUMBER_OF_PRESSES_UNTIL_BIG_CIRCLE_SPAWNS(10);
+const int BIG_CIRCLE_RADIUS(25);
+
+int main() {
+
+	return 0;
+}
